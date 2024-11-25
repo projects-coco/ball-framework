5938ad2d380cf03ff658eabf45dacca1b649e742
GeonHwan Cha <geonhwan.cha@clroot.io>
2024-11-25 16:36:33 +0900

    Add initial Gradle setup
    Introduced Gradle build system with Kotlin DSL. Configured project dependencies, Kotlin code style, and various plugins. Added gitignore settings for common development environments.

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinter)
+}
+
+group = "org.coco"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val deps = libs
+
+allprojects {
+    apply {
+        plugin("java")
+        plugin(
+            deps.plugins.kotlin.jvm
+                .get()
+                .pluginId,
+        )
+        plugin(
+            deps.plugins.kotlinter
+                .get()
+                .pluginId,
+        )
+    }
+
+    java.sourceCompatibility = JavaVersion.VERSION_21
+    java.targetCompatibility = JavaVersion.VERSION_21
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(deps.kotlin.reflect)
+        implementation(deps.kotlinx.coroutines.core)
+        implementation(deps.kotlinx.coroutines.reactor)
+        implementation(deps.arrow.core)
+        testImplementation(deps.bundles.kotest)
+        testImplementation(deps.mockk)
+        testImplementation(kotlin("test"))
+    }
+
+    tasks {
+        formatKotlinMain {
+            exclude { it.file.path.contains("generated") }
+        }
+        lintKotlinMain {
+            dependsOn("formatKotlinMain")
+            exclude { it.file.path.contains("generated") }
+        }
+        test {
+            useJUnitPlatform()
+        }
+    }
+}
