6c1974085b064e4822dcdd886654d70863ec2708
GeonHwan Cha <geonhwan.cha@clroot.io>
2024-11-27 16:01:01 +0900

    Refactor Arrow extensions and add suspend logic support
    Revised typealias definitions for Logic and added SuspendLogic for supporting suspendable operations. Introduced inline functions for 'logic' and 'suspendLogic' to utilize type inference and enhanced error handling mechanisms with new suspend functions.

--- domain/src/main/kotlin/org/coco/domain/core/ArrowExtension.kt
+++ domain/src/main/kotlin/org/coco/domain/core/ArrowExtension.kt
@@ -1,14 +1,20 @@
 package org.coco.domain.core
 
 import arrow.core.Option
-import arrow.core.raise.Raise
-import arrow.core.raise.mapError
+import arrow.core.identity
+import arrow.core.raise.*
 import kotlin.experimental.ExperimentalTypeInference
 
-typealias Logic<E, A> = Raise<E>.() -> A
+typealias Logic<E, A> = EagerEffect<E, A>
+typealias SuspendLogic<E, A> = Effect<E, A>
 
+@Suppress("NOTHING_TO_INLINE")
 @OptIn(ExperimentalTypeInference::class)
-fun <E, A> logic(@BuilderInference block: Logic<E, A>): Logic<E, A> = block
+inline fun <E, A> logic(@BuilderInference noinline block: Logic<E, A>): Logic<E, A> = block
+
+@Suppress("NOTHING_TO_INLINE")
+@OptIn(ExperimentalTypeInference::class)
+inline fun <E, A> suspendLogic(@BuilderInference noinline block: SuspendLogic<E, A>): SuspendLogic<E, A> = block
 
 context(Raise<E2>)
 fun <E1, E2, A> Logic<E1, A>.bindOrRaise(transform: (E1) -> (E2)): A = mapError { transform(it) }.bind()
@@ -16,5 +22,13 @@ fun <E1, E2, A> Logic<E1, A>.bindOrRaise(transform: (E1) -> (E2)): A = mapError 
 context(Raise<Nothing>)
 fun <E1, A> Logic<E1, A>.bindOrThrow(transform: (E1) -> (LogicError)): A = mapError { throw transform(it) }.bind()
 
+context(Raise<E2>)
+suspend fun <E1, E2, A> SuspendLogic<E1, A>.bindOrRaise(transform: (E1) -> E2): A =
+    fold({ raise(transform(it)) }, ::identity)
+
 @Suppress("UnusedReceiverParameter")
 fun <T> Nothing?.toOption(): Option<T> = Option.fromNullable(null)
+
+fun main() {
+    effect<Nothing, Unit> {}
+}
