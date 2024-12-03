b3ec675999d6615cbbfdb620aabd52453385f442
GeonHwan Cha <geonhwan.cha@clroot.io>
2024-12-03 17:01:56 +0900

    Refactor RepositoryBase and add JpaRepositoryHelper
    Refactored the RepositoryBase interface to simplify the function list and removed Spring Data interfaces. Introduced JpaRepositoryHelper, providing a concrete implementation using JpaRepository, enabling CRUD operations with transaction management. This change enhances modularity and code maintainability.

--- domain/src/main/kotlin/org/coco/domain/model/RepositoryBase.kt
+++ domain/src/main/kotlin/org/coco/domain/model/RepositoryBase.kt
@@ -2,23 +2,28 @@ package org.coco.domain.model
 
 import org.coco.domain.core.ErrorType
 import org.coco.domain.core.LogicError
-import org.springframework.data.domain.Example
-import org.springframework.data.domain.Sort
-import org.springframework.data.repository.ListCrudRepository
-import org.springframework.data.repository.ListPagingAndSortingRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.NoRepositoryBean
-import org.springframework.data.repository.query.QueryByExampleExecutor
+import java.util.*
 import kotlin.reflect.KClass
 
 class EntityNotFoundError(kClass: KClass<*>, id: BinaryId) :
     LogicError("${kClass.simpleName}(id=$id, hex=${id.toHexString()}) is not found.", ErrorType.NOT_FOUND)
 
 @NoRepositoryBean
-interface RepositoryBase<T : EntityBase> :
-    ListCrudRepository<T, BinaryId>, ListPagingAndSortingRepository<T, BinaryId>, QueryByExampleExecutor<T> {
-    override fun <S : T> findAll(example: Example<S>): MutableIterable<S>
+interface RepositoryBase<T : EntityBase> {
+    fun findById(id: BinaryId): Optional<T>
 
-    override fun <S : T> findAll(example: Example<S>, sort: Sort): MutableIterable<S>
+    fun findAll(): List<T>
+
+    fun findAll(ids: List<BinaryId>): List<T>
+
+    fun findAll(pageable: Pageable): Page<T>
+
+    fun save(entity: T): T
 
     fun update(id: BinaryId, modifier: (T) -> Unit)
+
+    fun delete(id: BinaryId)
 }
