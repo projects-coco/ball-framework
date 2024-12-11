53d7b08942b74a242abc4f734199408efab86a8b
GeonHwan Cha <geonhwan.cha@clroot.io>
2024-12-11 10:17:23 +0900

    Add Kotlin JDSL dependencies
    Implemented placeholders for user search methods in `UserRepository`. Updated Gradle dependencies to include Kotlin JDSL for JPQL support, rendering, and Spring Data JPA integration to facilitate advanced querying capabilities.

--- sample-project/src/main/kotlin/org/coco/example/infra/jpa/UserRepositoryImpl.kt
+++ sample-project/src/main/kotlin/org/coco/example/infra/jpa/UserRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package org.coco.example.infra.jpa
 
+import org.coco.domain.model.user.BasicUserSearchDto
 import org.coco.example.domain.model.user.User
 import org.coco.example.domain.model.user.UserRepository
 import org.coco.example.infra.jpa.model.user.UserDataModel
 import org.coco.example.infra.jpa.model.user.UserJpaRepository
 import org.coco.infra.jpa.JpaRepositoryHelper
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -21,5 +24,13 @@ class UserRepositoryImpl(
     override fun save(entity: User): User {
         val dataModel = UserDataModel.of(entity)
         return jpaRepository.save(dataModel).toEntity()
+    }
+
+    override fun search(searchDto: BasicUserSearchDto): List<User> {
+        TODO("Not yet implemented")
+    }
+
+    override fun search(searchDto: BasicUserSearchDto, pageable: Pageable): Page<User> {
+        TODO("Not yet implemented")
     }
 }
