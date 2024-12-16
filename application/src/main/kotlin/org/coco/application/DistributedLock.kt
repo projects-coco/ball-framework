119bde6a0c07206970bd2f465d1a90c654473290
ohjinhokor <ohjinho7@gmail.com>
2024-12-16 10:42:33 +0900

    fix: - DistributedLockAop 클래스 별도의 파일로 분리 - TxForAop -> TxAdvice로 통합

--- application/src/main/kotlin/org/coco/application/DistributedLock.kt
+++ application/src/main/kotlin/org/coco/application/DistributedLock.kt
@@ -1,14 +1,5 @@
 package org.coco.application
 
-import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.Around
-import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.reflect.MethodSignature
-import org.coco.domain.util.LockHandler
-import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
-import java.lang.reflect.Method
 import java.util.concurrent.TimeUnit
 
 @Target(AnnotationTarget.FUNCTION)
@@ -20,40 +11,3 @@ annotation class DistributedLock(
     val leaseTime: Long = 3L
 )
 
-@Aspect
-@Component
-class DistributedLockAop(
-    val lockHandler: LockHandler,
-    val txForAop: TxForAop,
-) {
-    val REDISSON_LOCK_PREFIX: String = "LOCK:"
-
-    @Around("@annotation(org.coco.application.DistributedLock)")
-    fun lock(joinPoint: ProceedingJoinPoint): Any {
-        val signature: MethodSignature = joinPoint.signature as MethodSignature
-        val method: Method = signature.getMethod()
-        val distributedLock: DistributedLock = method.getAnnotation(DistributedLock::class.java)
-
-        val key: String = REDISSON_LOCK_PREFIX + distributedLock.key
-
-        try {
-            val available = lockHandler.tryLock(key, distributedLock.waitTime, distributedLock.leaseTime)
-            if (!available) {
-                return false
-            }
-            return txForAop.proceed(joinPoint)
-        } catch (e: InterruptedException) {
-            throw InterruptedException()
-        } finally {
-            lockHandler.unlock(key)
-        }
-    }
-}
-
-@Component
-class TxForAop {
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    fun proceed(joinPoint: ProceedingJoinPoint): Any {
-        return joinPoint.proceed()
-    }
-}
--- application/src/main/kotlin/org/coco/application/TxAdvice.kt
+++ application/src/main/kotlin/org/coco/application/TxAdvice.kt
@@ -1,12 +1,18 @@
 package org.coco.application
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 
 @Component
 class TxAdvice {
     @Transactional
     fun <T> run(function: () -> T): T {
+        return function.invoke()
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun <T> runWithNewTransaction(function: () -> T): T {
         return function.invoke()
     }
 }
