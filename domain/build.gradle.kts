a9ad6c0c73ff07622d971b8168552af395161e89
GeonHwan Cha <geonhwan.cha@clroot.io>
2024-12-16 15:33:07 +0900

    Remove Maven repository configuration from build scripts
    The Maven repository configuration has been removed across multiple `build.gradle.kts` files. This simplifies the build scripts by eliminating redundant repository settings. The change ensures a more consistent and centralized repository management approach.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -14,18 +14,6 @@ val localPropertiesFile = rootProject.file("local.properties")
 localProperties.load(FileInputStream(localPropertiesFile))
 
 publishing {
-    repositories {
-        maven {
-            val releasesRepoUrl = uri(localProperties["coco.repo.url.release"] as String)
-            val snapshotsRepoUrl = uri(localProperties["coco.repo.url.snapshot"] as String)
-            credentials {
-                username = localProperties["coco.repo.username"] as String
-                password = localProperties["coco.repo.password"] as String
-            }
-            url = if (rootVersion.toString().endsWith("RELEASE")) releasesRepoUrl else snapshotsRepoUrl
-        }
-    }
-
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
