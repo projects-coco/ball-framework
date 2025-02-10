a4b0f1c9ad771131e166ff7ff9647994dcd03c2c
GeonHwan Cha <geonhwan.cha@clroot.io>
2025-02-10 16:33:43 +0900

    Rename "name" field to "legalName" for clarity.
    Updated the field name in both `UserRepositoryImpl` and `BasicUserSearchDto` to ensure consistency and improve semantic accuracy. This change reflects the intent more clearly and aligns the data model with expected terminology.

--- domain/src/main/kotlin/org/coco/domain/model/user/BasicUserSearchDto.kt
+++ domain/src/main/kotlin/org/coco/domain/model/user/BasicUserSearchDto.kt
@@ -8,7 +8,7 @@ import org.coco.domain.model.SearchDto
 open class BasicUserSearchDto(
     open val id: Option<BinaryId> = null.toOption(),
     open val username: Option<String> = null.toOption(),
-    open val name: Option<String> = null.toOption(),
+    open val legalName: Option<String> = null.toOption(),
     open val phoneNumber: Option<String> = null.toOption(),
     open val active: Option<Boolean> = null.toOption(),
     open val loginCount: Option<Long> = null.toOption(),
