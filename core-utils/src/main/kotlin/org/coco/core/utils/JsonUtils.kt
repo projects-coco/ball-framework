78be202636e612cd8c1596847f4688a836dfd97f
GeonHwan Cha <geonhwan.cha@clroot.io>
2025-02-19 12:10:40 +0900

    Refactor JSON utilities and enhance Arrow integration
    Updated `JsonUtils` with improved serialization and deserialization configuration, adding Arrow module support and refactoring code for clarity and consistency. Enhanced tests to cover Arrow’s `Option` types and updated dependencies to include Arrow-Jackson integration.

--- core-utils/src/main/kotlin/org/coco/core/utils/JsonUtils.kt
+++ core-utils/src/main/kotlin/org/coco/core/utils/JsonUtils.kt
@@ -1,55 +1,56 @@
 package org.coco.core.utils
 
+import arrow.integrations.jackson.module.registerArrowModule
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.Reader
+import kotlin.reflect.KClass
 
 object JsonUtils {
-    private val objectMapper =
-        ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .registerModule(Jdk8Module())
-            .registerModule(JavaTimeModule())
-            .registerKotlinModule()
+    val objectMapper: ObjectMapper =
+        ObjectMapper().apply {
+            registerModule(Jdk8Module())
+            registerModule(JavaTimeModule())
+            registerKotlinModule()
+            registerArrowModule()
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        }
 
-    fun serialize(`object`: Any): String {
-        return objectMapper.writeValueAsString(`object`)
-    }
+    fun serialize(`object`: Any): String = objectMapper.writeValueAsString(`object`)
 
-    fun <T> deserialize(
+    fun <T : Any> deserialize(
         reader: Reader,
         typeReference: TypeReference<T>,
-    ): T {
-        return objectMapper.readValue(reader, typeReference)
-    }
+    ): T = objectMapper.readValue(reader, typeReference)
 
-    fun <T> deserialize(
+    fun <T : Any> deserialize(
         reader: Reader,
         clazz: Class<T>,
-    ): T {
-        return objectMapper.readValue(reader, clazz)
-    }
+    ): T = objectMapper.readValue(reader, clazz)
 
-    fun deserialize(jsonString: String): Map<String, Any> {
-        return objectMapper.readValue(jsonString)
-    }
+    fun deserialize(jsonString: String): Map<String, Any> = objectMapper.readValue(jsonString)
 
-    fun <T> deserialize(
+    fun <T : Any> deserialize(
+        jsonString: String,
+        clazz: KClass<T>,
+    ): T = objectMapper.readValue(jsonString, clazz.java)
+
+    fun <T : Any> deserialize(
         jsonString: String,
         clazz: Class<T>,
-    ): T {
-        return objectMapper.readValue(jsonString, clazz)
-    }
+    ): T = objectMapper.readValue(jsonString, clazz)
 
-    fun <T> deserialize(
+    fun <T : Any> deserialize(
         jsonString: String,
         typeReference: TypeReference<T>,
-    ): T {
-        return objectMapper.readValue(jsonString, typeReference)
-    }
+    ): T = objectMapper.readValue(jsonString, typeReference)
 }
--- core-utils/src/test/kotlin/org/coco/core/utils/JsonUtilsTest.kt
+++ core-utils/src/test/kotlin/org/coco/core/utils/JsonUtilsTest.kt
@@ -1,32 +1,58 @@
 package org.coco.core.utils
 
+import arrow.core.Option
+import arrow.core.toOption
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import org.coco.core.extension.toOption
 
-class JsonUtilsTest : FunSpec({
-    test("JsonUtils#serialize(Any)") {
-        val sample =
-            object {
-                @Suppress("unused")
-                val name = "coco"
-            }
-        JsonUtils.serialize(sample) shouldBe """{"name":"coco"}"""
-    }
+class JsonUtilsTest :
+    FunSpec({
+        test("JsonUtils#serialize(Any)") {
+            val sample =
+                object {
+                    @Suppress("unused")
+                    val name = "coco"
+                }
+            JsonUtils.serialize(sample) shouldBe """{"name":"coco"}"""
+        }
 
-    test("JsonUtils#deserialize(String, Class<T>)") {
-        val sample = """{"name":"coco"}"""
+        test("JsonUtils#deserialize(String, Class<T>)") {
+            val sample = """{"name":"coco"}"""
 
-        data class Sample(val name: String)
-
-        JsonUtils.deserialize(sample, Sample::class.java) shouldBe Sample("coco")
-    }
-
-    test("JsonUtils#deserialize(String)") {
-        val sample = """{"name":"coco"}"""
-
-        JsonUtils.deserialize(sample) shouldBe
-            mapOf(
-                "name" to "coco",
+            data class Sample(
+                val name: String,
             )
-    }
-})
+
+            JsonUtils.deserialize(sample, Sample::class.java) shouldBe Sample("coco")
+        }
+
+        test("JsonUtils#deserialize(String)") {
+            val sample = """{"name":"coco"}"""
+
+            JsonUtils.deserialize(sample) shouldBe
+                mapOf(
+                    "name" to "coco",
+                )
+        }
+
+        test("JsonUtils#deserialize() with arrow.core.option #1") {
+            data class Sample(
+                val name: Option<String> = null.toOption(),
+            )
+
+            val sample = """{"name":"coco"}"""
+
+            JsonUtils.deserialize(sample, Sample::class) shouldBe Sample(name = "coco".toOption())
+        }
+
+        test("JsonUtils#deserialize() with arrow.core.option #2") {
+            data class Sample(
+                val name: Option<String> = null.toOption(),
+            )
+
+            val sample = """{}"""
+
+            JsonUtils.deserialize(sample, Sample::class) shouldBe Sample(name = null.toOption())
+        }
+    })
